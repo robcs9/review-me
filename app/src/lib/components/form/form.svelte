88679e556385321b07fd3d710c15f5f9f424cf81
robcs9 <83130807+robcs9@users.noreply.github.com>
2024-10-13 20:31:36 -0300

    Adicionando scaffold

---
+++ app/src/lib/components/form/form.svelte
@@ -0,0 +1,24 @@
+<script>
+    
+</script>
+
+<div class="">
+    <label class="label" for="comment">
+        Comment
+        <textarea class="form-textarea" name="comment" id="comment" />
+    </label>
+    <div class="space-y-2">
+        <label class="flex items-center space-x-2">
+            <input class="radio" type="radio" checked name="radio-direct" value="1" />
+            <p>Option 1</p>
+        </label>
+        <label class="flex items-center space-x-2">
+            <input class="radio" type="radio" name="radio-direct" value="2" />
+            <p>Option 2</p>
+        </label>
+        <label class="flex items-center space-x-2">
+            <input class="radio" type="radio" name="radio-direct" value="3" />
+            <p>Option 3</p>
+        </label>
+    </div>
+</div>
