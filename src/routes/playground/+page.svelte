5b38a2608209d154806b6f864c654e06d008aad7
robcs9 <83130807+robcs9@users.noreply.github.com>
2025-03-31 09:39:49 -0300

    Atualizando playground

--- src/routes/playground/+page.svelte
+++ src/routes/playground/+page.svelte
@@ -2,6 +2,8 @@
 	import { enhance } from '$app/forms';
 	import type { ActionData } from '../$types';
 	import type { PageData, PageParentData } from './$types';
+	// import { Card } from '@skeletonlabs/skeleton'
+	
 
 	// Modal
 	import { Modal, getModalStore } from '@skeletonlabs/skeleton';
@@ -70,6 +72,9 @@
 	$: if (form?.fail) {
 		toastify.error(toastStore, 'Error!');
 	}
+	
+	const imgSrc =
+  'https://images.unsplash.com/photo-1463171515643-952cee54d42a?q=80&w=450&h=190&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
 </script>
 
 <div>
@@ -86,11 +91,11 @@
 </div>
  -->
 <!-- z-index and positioning testing -->
-<h1 class="h1 absolute z-[1000] bg-green-400 outline-double">All Good!</h1>
-<h1 class="h1 absolute z-[999] bg-blue-400">All Gucci!</h1>
+<!-- <h1 class="h1 absolute z-[1000] bg-green-400 outline-double">All Good!</h1>
+<h1 class="h1 absolute z-[999] bg-blue-400">All Gucci!</h1> -->
 
 <!-- Form debugging -->
-<form method="POST" class="flex flex-col items-center gap-6 p-4" use:enhance={handleSubmit}>
+<!-- <form method="POST" class="flex flex-col items-center gap-6 p-4" use:enhance={handleSubmit}>
 	<label for="f-comentario" class="">
 		<h4 class="h4">{comentario.label.toUpperCase()}</h4>
 	</label>
@@ -106,4 +111,32 @@
 	{#if form?.success}
 		<h1 class="h1">FORM SUCCESSFULLY SUBMITTED</h1>
 	{/if}
-</form>
+</form> -->
+
+<div class="m-4 h-[100vh] bg-orange-200 outline-dashed outline-teal-500">
+	<a
+  href="#"
+  class="card preset-filled-surface-100-900 border-[1px] border-surface-200-800 card-hover divide-surface-200-800 block max-w-md divide-y overflow-hidden"
+>
+  <!-- {/* Header */} -->
+  <header>
+    <img src={imgSrc} class="aspect-[21/9] w-full grayscale hue-rotate-90" alt="banner" />
+  </header>
+  <!-- {/* Article */} -->
+  <article class="space-y-4 p-4">
+    <div>
+      <h2 class="h6">Announcements</h2>
+      <h3 class="h3">Skeleton is Awesome</h3>
+    </div>
+    <p class="opacity-60">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aspernatur provident eveniet eligendi cumque consequatur tempore sint
+      nisi sapiente. Iste beatae laboriosam iure molestias cum expedita architecto itaque quae rem.
+    </p>
+  </article>
+  <!-- {/* Footer */} -->
+  <footer class="flex items-center justify-between gap-4 p-4">
+    <small class="opacity-60">By Alex</small>
+    <small class="opacity-60">On {new Date().toLocaleDateString()}</small>
+  </footer>
+</a>
+</div>
